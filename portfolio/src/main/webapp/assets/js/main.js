e28de16ea7d36f6c92afd1362709cadcfcbaa100
Angela <cheoalee@gmail.com>
2020-05-28 10:38:47 -0700

    Added necessary JSDocs, cleaned spacing, fixed hex capitalization, styled CodyHouse code.

--- portfolio/src/main/webapp/assets/js/main.js
+++ portfolio/src/main/webapp/assets/js/main.js
@@ -1,5 +1,10 @@
+// Horizontal Timeline - by CodyHouse.co.
+
 (function() {
-  // Horizontal Timeline - by CodyHouse.co
+ /**
+  * Create a horizontal timeline.
+  * @param {Element} element An element of the timeline.
+  */
   var HorizontalTimeline = function(element) {
 		this.element = element;
 		this.datesContainer = this.element.getElementsByClassName('cd-h-timeline__dates')[0];
@@ -15,8 +20,8 @@
 		
 		this.eventsMinDistance = 60; // min distance between two consecutive events (in px)
 		this.eventsMaxDistance = 200; // max distance between two consecutive events (in px)
-		this.translate = 0; // this will be used to store the translate value of this.line
-		this.lineLength = 0; //total length of this line
+		this.translate = 0; // this will be used to store the translate value of this line
+		this.lineLength = 0; // total length of this line
 		
 		// Store index of selected and previous selected dates.
 		this.oldDateIndex = Util.getIndexInArray(this.date, this.selectedDate);
@@ -26,8 +31,11 @@
 		initEvents(this);
   };
 
+ /**
+  * Set position of remaining timeline dates.
+  * @param {HorizontalTimeline} timeline The timeline to which to add the dates.
+  */
   function initTimeline(timeline) {
-  	// set dates left position
   	var left = 0;
 		for (var i = 0; i < timeline.dateValues.length; i++) { 
 			var j = (i == 0) ? 0 : i - 1;
@@ -43,18 +51,22 @@
 	    timeline.date[i].setAttribute('style', 'left:' + left+'px');
 		}
 		
-		// set line/filling line dimensions
+	// Set line/filling line dimensions.
     timeline.line.style.width = (left + timeline.eventsMinDistance)+'px';
 		timeline.lineLength = left + timeline.eventsMinDistance;
-		// reveal timeline
+		// Reveal timeline.
 		Util.addClass(timeline.element, 'cd-h-timeline--loaded');
 		selectNewDate(timeline, timeline.selectedDate);
 		resetTimelinePosition(timeline, 'next');
   };
 
+ /**
+  * Initialize events according to scrolling on timeline.
+  * @param {HorizontalTimeline} timeline The timeline on which to scroll.
+  */
   function initEvents(timeline) {
   	var self = timeline;
-		// click on arrow navigation
+		// Click on arrow navigation.
 		self.navigation[0].addEventListener('click', function(event){
 			event.preventDefault();
 			translateTimeline(self, 'prev');
@@ -64,7 +76,7 @@
 			translateTimeline(self, 'next');
 		});
 
-		//swipe on timeline
+		// Swipe on timeline.
 		new SwipeContent(self.datesContainer);
 		self.datesContainer.addEventListener('swipeLeft', function(event){
 			translateTimeline(self, 'next');
@@ -73,7 +85,7 @@
 			translateTimeline(self, 'prev');
 		});
 
-		//select a new event
+		// Select a new event.
 		for(var i = 0; i < self.date.length; i++) {
 			(function(i){
 				self.date[i].addEventListener('click', function(event){
@@ -88,6 +100,10 @@
 		}
   };
 
+ /**
+  * Update the fill on the timeline itself according to user scrolling.
+  * @param {HorizontalTimeline} timeline The timeline on which to update filling.
+  */
   function updateFilling(timeline) { // update fillingLine scale value
 		var dateStyle = window.getComputedStyle(timeline.selectedDate, null),
 			left = dateStyle.getPropertyValue("left"),
@@ -97,6 +113,10 @@
 		timeline.fillingLine.style.transform = 'scaleX('+(left/timeline.lineLength)+')';
 	};
 
+  /**
+   * @param {HorizontalTimeline} timeline The timeline to translate.
+   * @param {string} direction The direction in which to translate.
+   */
   function translateTimeline(timeline, direction) { // translate timeline (and date elements)
   	var containerWidth = timeline.datesContainer.offsetWidth;
   	if(direction) {
@@ -106,12 +126,16 @@
     if( timeline.translate > 0 ) timeline.translate = 0;
 
     timeline.line.style.transform = 'translateX('+timeline.translate+'px)';
-    // update the navigation items status (toggle inactive class)
+        // Update the navigation items status (toggle inactive class)
 		(timeline.translate == 0 ) ? Util.addClass(timeline.navigation[0], 'cd-h-timeline__navigation--inactive') : Util.removeClass(timeline.navigation[0], 'cd-h-timeline__navigation--inactive');
 		(timeline.translate == containerWidth - timeline.lineLength ) ? Util.addClass(timeline.navigation[1], 'cd-h-timeline__navigation--inactive') : Util.removeClass(timeline.navigation[1], 'cd-h-timeline__navigation--inactive');
   };
 
-	function selectNewDate(timeline, target) { // ned date has been selected -> update timeline
+  /**
+   * @param {HorizontalTimeline} timeline The timeline to translate.
+   * @param {string} direction The direction in which to translate.
+   */
+	function selectNewDate(timeline, target) { // new date has been selected -> update timeline
 		timeline.newDateIndex = Util.getIndexInArray(timeline.date, target);
 		timeline.oldDateIndex = Util.getIndexInArray(timeline.date, timeline.selectedDate);
 		Util.removeClass(timeline.selectedDate, 'cd-h-timeline__date--selected');
@@ -122,13 +146,21 @@
 		updateFilling(timeline);
 	};
 
-	function updateOlderEvents(timeline) { // update older events style
+  /**
+   * Update style of older events.
+   * @param {HorizontalTimeline} timeline The timeline on which to update.
+   */
+	function updateOlderEvents(timeline) {
 		for(var i = 0; i < timeline.date.length; i++) {
 			(i < timeline.newDateIndex) ? Util.addClass(timeline.date[i], 'cd-h-timeline__date--older-event') : Util.removeClass(timeline.date[i], 'cd-h-timeline__date--older-event');
 		}
 	};
 
-	function updateVisibleContent(timeline) { // show content of new selected date
+  /**
+   * Update timelien to show contents of newly selected dates.
+   * @param {HorizontalTimeline} timeline The timeline on which to update.
+   */
+	function updateVisibleContent(timeline) {
 		if (timeline.newDateIndex > timeline.oldDateIndex) {
 			var classEntering = 'cd-h-timeline__event--selected cd-h-timeline__event--enter-right',
 				classLeaving = 'cd-h-timeline__event--leave-left';
@@ -148,20 +180,34 @@
 		}
 	};
 
-	function resetAnimation(timeline) { // reset content classes when entering animation is over
+  /**
+   * Reset content classes when entering animation is over.
+   * @param {HorizontalTimeline} timeline The timeline on which to reset.
+   */
+	function resetAnimation(timeline) {
 		timeline.contentWrapper.style.height = null;
 		Util.removeClass(timeline.content[timeline.newDateIndex], 'cd-h-timeline__event--enter-right cd-h-timeline__event--enter-left');
 		Util.removeClass(timeline.content[timeline.oldDateIndex], 'cd-h-timeline__event--leave-right cd-h-timeline__event--leave-left');
 	};
 
-	function keyNavigateTimeline(timeline, direction) { // navigate the timeline using the keyboard
+  /**
+   * Navigate the timeline using the keyboard.
+   * @param {HorizontalTimeline} timeline The timeline on which to navigate.
+   * @param {string} direction The direction of navigation.
+   */
+	function keyNavigateTimeline(timeline, direction) {
 		var newIndex = (direction == 'next') ? timeline.newDateIndex + 1 : timeline.newDateIndex - 1;
 		if(newIndex < 0 || newIndex >= timeline.date.length) return;
 		selectNewDate(timeline, timeline.date[newIndex]);
 		resetTimelinePosition(timeline, direction);
 	};
 	
-	function resetTimelinePosition(timeline, direction) { // translate timeline according to new selected event position
+  /**
+   * Translate timeline according to new selected event position.
+   * @param {HorizontalTimeline} timeline The timeline on which to reset.
+   * @param {string} direction The direction of translation.
+   */
+    function resetTimelinePosition(timeline, direction) {
 		var eventStyle = window.getComputedStyle(timeline.selectedDate, null),
 			eventLeft = Number(eventStyle.getPropertyValue('left').replace('px', '')),
 			timelineWidth = timeline.datesContainer.offsetWidth;
@@ -172,7 +218,11 @@
     }
   };
 
-  function parseDate(timeline) { // get timestamp value for each date
+ /**
+  * Get timestamp value for each date.
+  * @param {HorizontalTimeline} timeline The timeline whose values to retrieve.
+  */
+  function parseDate(timeline) {
 		var dateArrays = [];
 		for(var i = 0; i < timeline.date.length; i++) {
 			var singleDate = timeline.date[i].getAttribute('data-date'),
@@ -181,7 +231,7 @@
 			if( dateComp.length > 1 ) { // both DD/MM/YEAR and time are provided
 				var dayComp = dateComp[0].split('/'),
 					timeComp = dateComp[1].split(':');
-			} else if( dateComp[0].indexOf(':') >=0 ) { // only time is provide
+			} else if( dateComp[0].indexOf(':') >=0 ) { // only time is provided
 				var dayComp = ["2000", "0", "0"],
 					timeComp = dateComp[0].split(':');
 			} else { // only DD/MM/YEAR
@@ -194,7 +244,11 @@
 	  return dateArrays;
   };
 
-  function calcMinLapse(timeline) { // determine the minimum distance among events
+ /**
+  * Determine the minimum distance among events.
+  * @param {HorizontalTimeline} timeline The timeline with events used for determination.
+  */
+  function calcMinLapse(timeline) {
 		var dateDistances = [];
 		for(var i = 1; i < timeline.dateValues.length; i++) { 
 	    var distance = daydiff(timeline.dateValues[i-1], timeline.dateValues[i]);
@@ -204,7 +258,12 @@
 		return (dateDistances.length > 0 ) ? Math.min.apply(null, dateDistances) : 86400000;
 	};
 
-	function daydiff(first, second) { // time distance between events
+  /**
+   * Calculate the time distance between two events.
+   * @param {number} first
+   * @param {number} second
+   */
+	function daydiff(first, second) {
 		return Math.round((second-first));
 	};
 
@@ -216,7 +275,7 @@
 		for(var i = 0; i < horizontalTimeline.length; i++) {
 			horizontalTimelineTimelineArray.push(new HorizontalTimeline(horizontalTimeline[i])); 
 		}
-		// navigate the timeline when inside the viewport using the keyboard
+		// Navigate the timeline when inside the viewport using the keyboard.
 		document.addEventListener('keydown', function(event){
 			if( (event.keyCode && event.keyCode == 39) || ( event.key && event.key.toLowerCase() == 'arrowright') ) {
 				updateHorizontalTimeline('next'); // move to next event
@@ -226,16 +285,18 @@
 		});
   };
 
+ /**
+  * @param {string} direction The direction in which to update.
+  */
   function updateHorizontalTimeline(direction) {
 		for(var i = 0; i < horizontalTimelineTimelineArray.length; i++) {
 			if(elementInViewport(horizontalTimeline[i])) keyNavigateTimeline(horizontalTimelineTimelineArray[i], direction);
 		}
   };
 
-  /*
-		How to tell if a DOM element is visible in the current viewport?
-		http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-	*/
+   /**
+    * @param {element} el A timeline element.
+    */
 	function elementInViewport(el) {
 		var top = el.offsetTop;
 		var left = el.offsetLeft;
