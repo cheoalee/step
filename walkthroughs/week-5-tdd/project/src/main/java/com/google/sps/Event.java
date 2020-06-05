2f6655b9cf7e99bcef93fcfbe20d8423d92a5e7c
Angela <cheoalee@gmail.com>
2020-06-05 03:51:19 +0000

    Passed all given tests for FindMeetingQuery.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/Event.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/Event.java
@@ -11,14 +11,14 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+ 
 package com.google.sps;
-
+ 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-
+ 
 /**
  * Event is the container class for when a specific group of people are meeting and are therefore
  * busy. Events are considered read-only.
@@ -27,7 +27,7 @@ public final class Event {
   private final String title;
   private final TimeRange when;
   private final Set<String> attendees = new HashSet<>();
-
+ 
   /**
    * Creates a new event.
    *
@@ -39,34 +39,48 @@ public final class Event {
     if (title == null) {
       throw new IllegalArgumentException("title cannot be null");
     }
-
+ 
     if (when == null) {
       throw new IllegalArgumentException("when cannot be null");
     }
-
+ 
     if (attendees == null) {
       throw new IllegalArgumentException("attendees cannot be null. Use empty array instead.");
     }
-
+ 
     this.title = title;
     this.when = when;
     this.attendees.addAll(attendees);
   }
-
+ 
   /**
    * Returns the human-readable name for this event.
    */
   public String getTitle() {
     return title;
   }
-
+ 
   /**
    * Returns the {@code TimeRange} for when this event occurs.
    */
   public TimeRange getWhen() {
     return when;
   }
-
+ 
+    /**
+   * Returns the start time for this event.
+   */
+  public int getStart() {
+    return when.start();
+  }
+ 
+    /**
+   * Returns the end time for this event.
+   */
+  public int getEnd() {
+    return when.end();
+  }
+ 
   /**
    * Returns a read-only set of required attendees for this event.
    */
@@ -75,19 +89,19 @@ public final class Event {
     // internal data.
     return Collections.unmodifiableSet(attendees);
   }
-
+ 
   @Override
   public int hashCode() {
     // For the hash code, just use the title. Most events "should" have different names and will
     // mainly be used as a way to skip the costly {@code equals()} call.
     return title.hashCode();
   }
-
+ 
   @Override
   public boolean equals(Object other) {
     return other instanceof Event && equals(this, (Event) other);
   }
-
+ 
   private static boolean equals(Event a, Event b) {
     // {@code attendees} must be a set for equals to work as expected. According to the {@code Set}
     // interface documentation, equals will check for set-equality across all set implementations.
--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -11,13 +11,186 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+ 
 package com.google.sps;
-
+ 
+import java.util.ArrayList;
 import java.util.Collection;
-
+import java.util.List;
+ 
 public final class FindMeetingQuery {
+ 
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    boolean attendeesExist = false;
+    List<TimeRange> meetingTimes = new ArrayList<TimeRange>();
+ 
+    /* GENERAL CASES */
+    // If duration is too long, return no slots
+    if(request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+      System.out.println("Duration is too long.");
+      return meetingTimes;
+    }
+ 
+    // If no conflicts (no events), return whole day
+    if (events.isEmpty()) {
+      System.out.println("No events; all slots work.");
+      return allSlots();
+    }
+ 
+    // If no meeting attendees, return whole day
+    attendeeCheck:
+    for (Event event:events) {
+      if (!event.getAttendees().isEmpty()) {
+        attendeesExist = true;
+        break attendeeCheck;
+      }
+    }
+ 
+    if (!attendeesExist) {
+      System.out.println("No attendees; all slots work.");
+      return allSlots();
+    }
+ 
+    // Now, you know:
+    // Attendees exist AND there are events AND the duration is acceptable (less than a day).
+    // Determine which events count (i.e. meeting invitees are
+    // attending those events).
+    List<Event> relevantEvents = countingEvents(events, request);
+ 
+    // If no attendees have any event conflicts; all slots work.
+    if (relevantEvents.size() < 1) {
+      System.out.println("No relevant event conflicts; all slots work.");
+      return allSlots();
+    }
+ 
+    /* Address contained events. */
+    relevantEvents = removeContainedEvents(relevantEvents);
+ 
+    /* Provide available meeting slots. */
+    return getAvailableSlots(relevantEvents, request);
+  }
+ 
+  /* FIND RELEVANT EVENTS */
+  // Figure out which events actually matter - i.e.
+  // a meeting invitee is attending the event - then sort them
+  // by start time as they are added to the relevant events list.
+  public List<Event> countingEvents(Collection<Event> events, MeetingRequest request) {
+    List<Event> relevantEvents = new ArrayList<Event>();
+    for (Event event:events) {
+      relevantAttendee:
+      for (String attendee:event.getAttendees()) {
+        // Check if a meeting attendee is attending the event.
+        if (request.getAttendees().contains(attendee)) {
+          int idx = 0;
+          System.out.println("RELEVANT ATTENDEE FOUND: One of the attendees, " + attendee + " has an event obligation.");
+            int relEventsSize = relevantEvents.size();
+            // If there exists other relevant events, compare them and sort accordingly.
+            if (relEventsSize > 0) {
+              System.out.println("COMPARING: Relevant events already contains an event. Comparing...");
+              System.out.println("EXISTING REL EVENT START: " + relevantEvents.get(idx).getStart());
+              System.out.println("THIS REL EVENT START: " + event.getStart());
+              while (idx < relEventsSize && TimeRange.ORDER_BY_START.compare(event.getWhen(), relevantEvents.get(idx).getWhen()) > 0) {
+                idx++;
+              }
+            }
+          System.out.println("ADDING RELEVANT EVENT: Event added to relevantEvents at index " + idx + "\n");
+          relevantEvents.add(idx, event);
+          break relevantAttendee;
+        }
+      }
+    }
+    return relevantEvents;
+  }
+ 
+  /* CONTAINED EVENTS */
+  // Account for contained events -
+  // that is, for each event, loop through previous events
+  // to see if this event is contained wholly in the previous event.
+  // If so, remove it - keep events to remove in a list.
+  // Start from the beginning.
+  public List<Event> removeContainedEvents(List<Event> relevantEvents) {
+    int currEventIdx = 0;
+    int eventIdx = 0;
+    List<Event> containedEvents = new ArrayList<Event>();
+    while (currEventIdx < relevantEvents.size()) {
+      System.out.println("Checking for contained events...");
+      while (eventIdx > 0) {
+        Event currEvent = relevantEvents.get(currEventIdx);
+        if (relevantEvents.get(eventIdx - 1).getWhen().contains(currEvent.getWhen())) {
+          System.out.println("Contained event found!");
+          containedEvents.add(currEvent);
+          break;
+        }
+        eventIdx--;
+      }
+      currEventIdx++;
+      eventIdx = currEventIdx;
+    }
+ 
+    // Remove contained events.
+    for (Event containedEvent:containedEvents) {
+      System.out.println("Removing contained event.");
+      relevantEvents.remove(containedEvent);
+    }
+    return relevantEvents;
+  }
+ 
+  /* FINAL STEP: PROVIDE MEETING TIMES */
+  public List<TimeRange> getAvailableSlots(List<Event> relevantEvents, MeetingRequest request) {
+    List<TimeRange> meetingSlots = new ArrayList<TimeRange>();
+    // Add time ranges accordingly
+    int eventIdx = 0;
+    int lastEventIdx = relevantEvents.size() - 1;
+    int start = 0;
+    int end = 0;
+    // TODO: Figure out why duration is a long in the first place.
+    int requestDuration = (int) request.getDuration();
+    for (Event relEvent:relevantEvents) {
+      System.out.println("TRACKED: Event lasting from " + relEvent.getStart() + " to " + relEvent.getEnd());
+      if (eventIdx == 0) {
+        start = TimeRange.START_OF_DAY;
+        end = relEvent.getStart();
+        // Don't allow user to create an event from 0 to 0 - ADD STIPULATION FOR MEETING DURATION > 1.
+        if (logicCheck(start, end, requestDuration)) {
+          meetingSlots.add(TimeRange.fromStartEnd(start, end, false));
+          System.out.println("TIME ADDED (START): From " + start + " to " + end);
+        }
+      } else {
+        int prevEventIdx = eventIdx - 1;
+        start = relevantEvents.get(prevEventIdx).getEnd();
+        end = relEvent.getStart();
+        if (logicCheck(start, end, requestDuration)) {
+          meetingSlots.add(TimeRange.fromStartEnd(start, end, false));
+          System.out.println("TIME ADDED (BTWN): From " + start + " to " + end);
+        }
+      }
+      if (eventIdx == lastEventIdx) {
+        start = relEvent.getEnd();
+        end = TimeRange.END_OF_DAY;
+        if (logicCheck(start, end, requestDuration)) {
+          meetingSlots.add(TimeRange.fromStartEnd(start, end, true));
+          System.out.println("TIME ADDED (END): From " + start + " to " + end);
+        }
+      }
+      System.out.println("\n");
+      eventIdx++;
+    }
+    return meetingSlots;
+  }
+ 
+  // Returns the entire day
+  public Collection<TimeRange> allSlots() {
+    List<TimeRange> meetingTimes = new ArrayList<TimeRange>();
+    meetingTimes.add(TimeRange.WHOLE_DAY);
+    return meetingTimes;
+  }
+ 
+  // Ensures that meeting suggestions make
+  // logical sense - that is, end > start
+  // and the duration is encompassed.
+  // In place for edge cases as a result of
+  // checks for overlaps.
+  public boolean logicCheck(int start, int end, int duration) {
+    return (start < end) && (start + duration <= end);
   }
 }
